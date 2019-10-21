6561033765f935749fac5d5be0a392df1e919562
blablabluomie <omieblablablu2996@gmail.com>
2019-10-21 22:04:48 +0530

    Backend: Server started with MongoDb connection

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+//importante
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Setup Connection of database
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection successful!");
+});
+
+//Setup Routes
+
+//const exercisesRouter = require("./routes/exercises");
+//const usersRouter = require("./routes/users");
+
+//app.use("/exercises", exercisesRouter);
+//app.use("/users", usersRouter);
+
+//Setup the listener
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
